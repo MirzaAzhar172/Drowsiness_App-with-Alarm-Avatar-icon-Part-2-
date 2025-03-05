6cf29f8ffae8e17d13803bdf7a7f0e7e87ebbc48
MirzaAzhar172 <130655399+MirzaAzhar172@users.noreply.github.com>
2025-03-05 10:59:28 +0800

    Create settings_page.dart

---
+++ lib/screens/settings_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  final bool isCameraEnabled;
+  final ValueChanged<bool> onCameraToggleChanged;
+
+  SettingsPage({Key? key, required this.isCameraEnabled, required this.onCameraToggleChanged})
+      : super(key: key);
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool _isCameraEnabled = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _isCameraEnabled = widget.isCameraEnabled;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/logo.png', // Gantikan dengan path logo anda
+                    width: 80,
+                    height: 80,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'DROWY APP',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Aktifkan atau nonaktifkan kamera dan pengesanan pada aplikasi ini.',
+                    style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 16),
+                  Divider(
+                    color: Colors.grey[400], // Warna garis pembahagi
+                    thickness: 1.0, // Ketebalan garis
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Camera/Detector',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Switch(
+                        value: _isCameraEnabled,
+                        onChanged: (value) {
+                          setState(() {
+                            _isCameraEnabled = value;
+                          });
+                          widget.onCameraToggleChanged(value);
+                        },
+                        activeColor: Colors.green,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
